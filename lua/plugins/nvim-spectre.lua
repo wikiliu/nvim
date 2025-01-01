73ebfe10f452a41068adfdcf4a7a09cdfe308b17
wiki <wikiliu66@outlook.com>
2025-01-01 21:27:06 +0800

    refine color

--- lua/plugins/nvim-spectre.lua
+++ lua/plugins/nvim-spectre.lua
@@ -1,10 +1,10 @@
 return {
-  "nvim-pack/nvim-spectre",
-  dependencies = { "nvim-lua/plenary.nvim" },
-
-  lazy = true,
-  opts = function(_, opts)
-    opts.previewWindow.border = "rounded"
-    return opts
-  end,
+  -- "nvim-pack/nvim-spectre",
+  -- dependencies = { "nvim-lua/plenary.nvim" },
+  --
+  -- lazy = true,
+  -- opts = function(_, opts)
+  --   opts.previewWindow.border = "rounded"
+  --   return opts
+  -- end,
 }
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -21,6 +21,7 @@ return {
             desc = "Close terminal and focus last window",
           },
         },
+        border = "double",
       },
     },
   },
