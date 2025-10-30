36094964bbcec10a326aa20e943537dbea397d38
wikiliu <wikiliu@outlook.com>
2025-10-30 18:12:55 +0800

    add Custom scp and remove gdb support.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,7 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-local dap = require("dap")
 vim.g.lazyvim_picker = "telescope"
 vim.g.lazyvim_cmp = "nvim-cmp"
 
@@ -19,31 +18,30 @@ vim.g.clipboard = {
   cache_enabled = 0,
 }
 
-
-dap.configurations.cpp = {
-  {
-    name = "Launch file",
-    type = "cppdbg",
-    request = "launch",
-    program = function()
-      return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-    end,
-    cwd = "${workspaceFolder}",
-    stopAtEntry = true,
-  },
-  {
-    MIMode = "gdb",
-    miDebuggerServerAddress = "10.30.16.114:1234", -- 修改为你的gdbserver地址和端口
-    miDebuggerPath = "/usr/bin/gdb", -- 修改为你的gdb路径
-    setupCommands = {
-      {
-        description = "Enable pretty-printing for gdb",
-        text = "-enable-pretty-printing",
-        ignoreFailures = false,
-      },
-    },
-  },
-}
+-- dap.configurations.cpp = {
+--   {
+--     name = "Launch file",
+--     type = "cppdbg",
+--     request = "launch",
+--     program = function()
+--       return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+--     end,
+--     cwd = "${workspaceFolder}",
+--     stopAtEntry = true,
+--   },
+--   {
+--     MIMode = "gdb",
+--     miDebuggerServerAddress = "10.30.16.114:1234", -- 修改为你的gdbserver地址和端口
+--     miDebuggerPath = "/usr/bin/gdb", -- 修改为你的gdb路径
+--     setupCommands = {
+--       {
+--         description = "Enable pretty-printing for gdb",
+--         text = "-enable-pretty-printing",
+--         ignoreFailures = false,
+--       },
+--     },
+--   },
+-- }
 
 vim.opt.shiftwidth = 4
 vim.o.pumblend = 0
