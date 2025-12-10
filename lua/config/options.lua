5ff7172766e72f5eab0a0a76dec359babf8e32c3
wikiliu <wikiliu@outlook.com>
2025-12-10 11:26:32 +0800

    update for lazyvim-15.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -49,9 +49,7 @@ vim.opt.shiftwidth = 4
 vim.o.pumblend = 0
 vim.g.autoformat = false
 vim.o.clipboard = "unnamedplus"
+vim.opt.signcolumn = "yes"
 
-require("lazyvim.util").lsp.on_attach(function()
-  vim.opt.signcolumn = "yes"
-end)
 vim.cmd([[autocmd VimEnter * lua require('utils.select-dir').load_dir()]])
 vim.cmd([[autocmd VimLeave * lua require('utils.select-dir').save_dir()]])
